8cb76af16b5f1990f98fd0630b81cbd753cb31ed
zulkiflijufri <zulkiflijufri54@gmail.com>
2020-12-31 20:09:13 +0800

    favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title')</title>
+    <link rel="icon" type="image/png" href="/img/favicon.png" />
     <link rel="stylesheet" href="/assets/css/tailwind.css">
 </head>
 
